e112fd05a391d370feec952a4434942328f054cf
Harsh <harshit.sparx002@gmail.com>
2026-04-08 21:51:58 +0530

    trytrytyr

--- hotel_project/booking/urls.py
+++ hotel_project/booking/urls.py
@@ -1,8 +1,10 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('rooms/', views.rooms, name='rooms'),
-    path('book/', views.book_room, name='book_room'),  # 🔥 ye important
+    path('book/', views.book_room, name='book_room'),
 ]
+
