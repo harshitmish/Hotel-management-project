c3a9cfcefdc3459c22032f1949899ee41b0045ba
Harsh <harshit.sparx002@gmail.com>
2026-04-08 21:23:34 +0530

    server error fix

--- hotel_project/hotel_project/urls.py
+++ hotel_project/hotel_project/urls.py
@@ -3,5 +3,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('booking.urls')),
-]
+    path('', include('hotel_project.booking.urls')),
