fc3b28f4e014dca8baf899418ea0ca9d3cd5e57b
Harsh <harshit.sparx002@gmail.com>
2026-04-22 05:08:35 +0530

    sendgrid setup

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -139,14 +139,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_TIMEOUT = 5
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
