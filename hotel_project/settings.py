ba22b1c07fdc2f26c49f83954485ceaf35f85a44
Harsh <harshit.sparx002@gmail.com>
2026-04-22 12:43:08 +0530

    nahopayega

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -147,6 +147,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'cafwebsitecontrol@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_app_password'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 DEFAULT_FROM_EMAIL = 'cafwebsitecontrol@gmail.com'
